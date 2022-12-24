4ee7a4405ae6370f3a475cd81742fd779d358a80
Brigham Andersen <brighamband@gmail.com>
2022-12-24 14:53:32 -0700

    Final touches

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,4 @@
-function enableEditMode(todoId) { 
-  const todoContent = document.querySelector(`#todo-content-${todoId}`);
-  todoContent.style.display = 'none';
-  const editForm = document.querySelector(`#edit-form-${todoId}`);
-  editForm.style.display = 'flex';
-}
-
-function disableEditMode(todoId) { 
-  const todoContent = document.querySelector(`#todo-content-${todoId}`);
-  todoContent.style.display = 'inline';
-  const editForm = document.querySelector(`#edit-form-${todoId}`);
-  editForm.style.display = 'none';
+function clearInput(todoId, todoContent) {
+  const editInput = document.querySelector(`#edit-input-${todoId}`);
+  editInput.value = todoContent;
 }
