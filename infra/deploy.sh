d3a6186852d7372878660fbf0b7e8ecf777aab0c
Brigham Andersen <brighambandersen@gmail.com>
2025-09-22 22:58:03 -0600

    Add deployment info

---
+++ infra/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Deploying flask-todo"
+
+sudo ln -sf /home/brig/code/flask-todo/infra/systemd/flask-todo.service /etc/systemd/system/flask-todo.service
+
+sudo ln -sf /home/brig/code/flask-todo/infra/nginx/flask-todo.conf /etc/nginx/conf.d/flask-todo.conf
+
+sudo systemctl daemon-reload
+sudo systemctl enable flask-todo.service
+sudo systemctl restart flask-todo.service
+
+sudo nginx -t
+sudo systemctl reload nginx
+
+echo "Deployment complete for flask-todo"
