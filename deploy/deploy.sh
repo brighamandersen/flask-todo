d705bd442d8378e4d51347ea1ec7e651e7814196
Brigham Andersen <brighambandersen@gmail.com>
2025-10-01 04:21:33 +0000

    get flask todo running

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,15 +3,19 @@ set -euo pipefail
 
 echo "Deploying flask-todo"
 
-sudo ln -sf /home/brig/code/flask-todo/deploy/systemd.service /etc/systemd/system/flask-todo.service
+# nginx
 
-sudo ln -sf /home/brig/code/flask-todo/deploy/nginx.conf /etc/nginx/conf.d/flask-todo.conf
+sudo cp /home/brig/code/flask-todo/deploy/nginx.conf /etc/nginx/conf.d/flask-todo.conf
+
+sudo nginx -t
+sudo systemctl reload nginx
+
+# systemd
+
+sudo cp /home/brig/code/flask-todo/deploy/systemd.service /etc/systemd/system/flask-todo.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable flask-todo.service
 sudo systemctl restart flask-todo.service
-
-sudo nginx -t
-sudo systemctl reload nginx
 
 echo "Deployment complete for flask-todo"
