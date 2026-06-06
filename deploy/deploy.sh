7534d0d7b10f62ac849a025986ad754148fb5ca2
Brigham Andersen <brighambandersen@gmail.com>
2026-06-05 23:52:56 -0600

    deploy ~/dev changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,14 +5,14 @@ echo "Deploying flask-todo"
 
 # nginx
 
-sudo cp /home/brig/code/flask-todo/deploy/nginx.conf /etc/nginx/conf.d/flask-todo.conf
+sudo cp /home/brig/dev/flask-todo/deploy/nginx.conf /etc/nginx/conf.d/flask-todo.conf
 
 sudo nginx -t
 sudo systemctl reload nginx
 
 # systemd
 
-sudo cp /home/brig/code/flask-todo/deploy/systemd.service /etc/systemd/system/flask-todo.service
+sudo cp /home/brig/dev/flask-todo/deploy/systemd.service /etc/systemd/system/flask-todo.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable flask-todo.service
